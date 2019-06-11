26b21d8ec7bdad8994b0c9773df85732fd441cb2
joethorley <joe@poissonconsulting.ca>
2019-06-11 06:34:48 -0700

    rename model to code

--- R/generate-data.R
+++ R/generate-data.R
@@ -1,14 +1,14 @@
-model_to_data_block <- function(model) {
-  str_replace(model, "model\\s*[{]", "data {") %>% 
+model_to_data_block <- function(code) {
+  str_replace(code, "model\\s*[{]", "data {") %>% 
     str_replace("[}]\\s*$", "}\nmodel {\n  dummy <- 0 \n}")
 }
 
-set_parameters <- function(model, parameters) {
+set_parameters <- function(code, parameters) {
   for(i in seq_along(parameters)) {
     par <- parameters[i]
     pattern <- str_c(names(par), "\\s*~\\s*[^\n}]+") 
     replacement <- str_c(names(par), " <- ", par)
-    model %<>% str_replace(pattern, replacement)
+    code %<>% str_replace(pattern, replacement)
   }
-  model
+  code
 }
