68c9b7ded5d5ce0ea43ef00f88b94c5aa813c0fd
joethorley <joe@poissonconsulting.ca>
2019-06-11 06:28:17 -0700

    basic set_parameters

--- R/generate-data.R
+++ R/generate-data.R
@@ -2,3 +2,13 @@ model_to_data_block <- function(model) {
   str_replace(model, "model\\s*[{]", "data {") %>% 
     str_replace("[}]\\s*$", "}\nmodel {\n  dummy <- 0 \n}")
 }
+
+set_parameters <- function(model, parameters) {
+  for(i in seq_along(parameters)) {
+    par <- parameters[i]
+    pattern <- str_c(names(par), "\\s*~\\s*[^\n]+") 
+    replacement <- str_c(names(par), " <- ", par)
+    model %<>% str_replace(pattern, replacement)
+  }
+  model
+}
--- tests/testthat/test-generate-data.R
+++ tests/testthat/test-generate-data.R
@@ -7,4 +7,17 @@ test_that("model_to_data_block", {
                    "data { x <- 1 }\nmodel {\n  dummy <- 0 \n}")
   expect_identical(model_to_data_block("model  { for(i in 1:n) {b[i] ~ 1}}"), 
                    "data { for(i in 1:n) {b[i] ~ 1}}\nmodel {\n  dummy <- 0 \n}")
+  expect_identical(model_to_data_block("
+  model{
+    for(i in 1:n) {
+      b[i] ~ 1
+    }
+  }"), 
+  "\n  data {\n    for(i in 1:n) {\n      b[i] ~ 1\n    }\n  }\nmodel {\n  dummy <- 0 \n}")
 })
+
+test_that("set_parameters", {
+  expect_identical(set_parameters("alpha ~ dunif(0, 1)\n", c(alpha = 0.5)), 
+                   "alpha <- 0.5\n")
+})
+
